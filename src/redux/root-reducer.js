f996494217325cf4f1b8bfd1226235df076eb429
Matt McDonald <mcdonamj087@gmail.com>
2021-01-26 22:59:43 -0600

    Added redux persist to store saved icons in localstorage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import symbolsReducer from './symbols/symbols.reducer';
 import activeCategoryReducer from './active-category/active-category.reducer';
@@ -6,10 +8,18 @@ import activeFormatReducer from './active-format/active-format.reducer';
 import searchValueReducer from './search-value/search-value.reducer';
 import savedIconsReducer from './saved-icons/saved-icons.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['savedIcons'],
+};
+
+const rootReducer = combineReducers({
   symbols: symbolsReducer,
   activeCategory: activeCategoryReducer,
   activeFormat: activeFormatReducer,
   searchValue: searchValueReducer,
   savedIcons: savedIconsReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
